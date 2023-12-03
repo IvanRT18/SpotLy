c66f555c243e4ddf9848e84f8f847b1dcb050eda
HazaelTorr <hazaeltorres09@gmail.com>
2023-12-03 12:50:51 -0600

    Vista ajustes 50% - Botón dark modo solo front, formulario edit usuario solo front

--- lib/ajustes.dart
+++ lib/ajustes.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_local_variable
+// ignore_for_file: unused_local_variable, deprecated_member_use
 
 import 'package:flutter/material.dart';
 import 'package:proyecto_movil/drawer.dart';
@@ -49,47 +49,293 @@ class _AjustesState extends State<Ajustes> {
         ],
       ),
       drawer: const DrawerApp(),
-      body: SizedBox(
-        width: widthDevice,
-        height: heightDevice,
-        child: Column(
-          children: [
-            // ElevatedButton(
-            //   onPressed: () {
-            //     setState(() {});
-            //   },
-            //   child: Text("Obtener usuarios"),
-            // ),
-            Expanded(
-              child: FutureBuilder(
-                future: getUsuarios(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return ListView.builder(
-                      itemCount: snapshot.data?.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Card(
-                          child: Column(
-                            children: [
-                              // Text(snapshot.data?[index]['id_usuario']),
-                              Text(snapshot.data?[index]['usuario']),
-                              Text(snapshot.data?[index]['correo']),
-                              Text(snapshot.data?[index]['password']),
-                            ],
-                          ),
-                        );
-                      },
-                    );
-                  } else {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                },
-              ),
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 15),
+              DarkModeToggle(),
+              MyDropdownMenu(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DarkModeToggle extends StatefulWidget {
+  @override
+  _DarkModeToggleState createState() => _DarkModeToggleState();
+}
+
+class _DarkModeToggleState extends State<DarkModeToggle> {
+  bool _isDarkMode = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            'Modo oscuro',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+          ),
+          Switch(
+            value: _isDarkMode,
+            onChanged: (value) {
+              setState(() {
+                _isDarkMode = value;
+                // Puedes añadir lógica para cambiar entre modos claro y oscuro aquí
+              });
+            },
+            activeTrackColor: Colors.grey[700],
+            activeColor: Colors.blueAccent,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyDropdownMenu extends StatefulWidget {
+  @override
+  _MyDropdownMenuState createState() => _MyDropdownMenuState();
+}
+
+class _MyDropdownMenuState extends State<MyDropdownMenu> {
+  bool _isExpanded = false;
+
+  final mailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final userController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          InkWell(
+            onTap: () {
+              setState(() {
+                _isExpanded = !_isExpanded;
+              });
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
+                  child: Text(
+                    'Editar usuario',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Icon(_isExpanded
+                    ? Icons.keyboard_arrow_up
+                    : Icons.keyboard_arrow_down),
+              ],
             ),
-          ],
-        ),
+          ),
+          if (_isExpanded)
+            Column(
+              children: [
+                // Usuario
+                TextFormField(
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 132, 132, 128),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  controller: userController,
+                  textAlign: TextAlign.left,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 10),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: const BorderSide(
+                        width: 2,
+                        color: Color(0x8C848480),
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: const BorderSide(
+                        width: 20,
+                        color: Color(0x8C848480),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: const BorderSide(
+                        width: 2,
+                        color: Color(0x8C848480),
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: "Usuario",
+                    hintStyle: const TextStyle(
+                      color: Color.fromARGB(255, 132, 132, 128),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    errorText: 'Debe de escribir un nombre de usuario válido',
+                  ),
+                ),
+                // Correo
+                TextFormField(
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 132, 132, 128),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  controller: mailController,
+                  textAlign: TextAlign.left,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 10),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: const BorderSide(
+                        width: 2,
+                        color: Color(0x8C848480),
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: const BorderSide(
+                        width: 20,
+                        color: Color(0x8C848480),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: const BorderSide(
+                        width: 2,
+                        color: Color(0x8C848480),
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: "Correo",
+                    hintStyle: const TextStyle(
+                      color: Color.fromARGB(255, 132, 132, 128),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    errorText: 'Debe de escribir un correo válido',
+                  ),
+                ),
+                // Contraseña
+                TextFormField(
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 132, 132, 128),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  controller: passwordController,
+                  textAlign: TextAlign.left,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(
+                          width: 2,
+                          color: Color(0x8C848480),
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(
+                          width: 2,
+                          color: Color(0x8C848480),
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(
+                          width: 2,
+                          color: Color(0x8C848480),
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: "Contraseña",
+                      hintStyle: const TextStyle(
+                        color: Color.fromARGB(255, 132, 132, 128),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      errorText:
+                          'Debe de ser una contraseña mayor a 6 caracteres'),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    // Botón Aceptar
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          primary: rojoApp, // Fondo rojo para el botón Aceptar
+                          onPrimary: Colors
+                              .white, // Texto blanco para el botón Aceptar
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                8.0), // Bordes redondeados
+                          ),
+                        ),
+                        child: const Text(
+                          'Aceptar',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 16), // Separación entre los botones
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors
+                              .white, // Fondo blanco para el botón Cancelar
+                          onPrimary:
+                              grisApp, // Texto gris para el botón Cancelar
+                          side: const BorderSide(color: grisApp), // Borde gris
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                8.0), // Bordes redondeados
+                          ),
+                        ),
+                        child: const Text(
+                          'Cancelar',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+        ],
       ),
     );
   }
--- lib/utils/constantes.dart
+++ lib/utils/constantes.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const rojoApp = Color(0xFFFC3C4A);
 const verdeApp = Color(0xFF00B978);
+const grisApp = Color(0xFF4D4D4D);
